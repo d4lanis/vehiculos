0b06e20f59d057e4fefc0f6ac1a60abe87b2d8b3
d4lanis <daniel.alanis.hdz@gmail.com>
2021-06-03 17:54:37 -0500

    mejora de aspecto visual de denunciantes

--- resources/views/denunciantes/index.blade.php
+++ resources/views/denunciantes/index.blade.php
@@ -9,11 +9,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
-
-<a href="{{route('denunciantes.create')}}" class="btn btn-success">Nuevo</a>
+<div class="container-xl">
+    <a href="{{route('denunciantes.create')}}" class="btn btn-success">Nuevo</a>
 <br><br>
 <table id="denunciantes" class="table table-striped table-bordered">
-    <thead>
+    <thead class="table-info">
         <tr>
             <th>NOMBRE</th>
             <th>PATERNO</th>
@@ -68,8 +68,62 @@
 
 <script>
     $(document).ready( function () {
-    $('#denunciantes').DataTable({
-        "lengthMenu": [[5,10,50,100,-1],[5,10,50,100,"All"]]
+        $('#denunciantes').DataTable({            
+            "lengthMenu": [[5,10,50,100,-1],[5,10,50,100,"Todos"]],
+            "scrollY":        "300px",
+            "scrollX":        true,
+            "scrollCollapse": true,
+            "responsive": true
+        });
     });
-    });
+
+    $.extend( true, $.fn.dataTable.defaults, {
+    "language": {
+        "decimal": ",",
+        "thousands": ".",
+        "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "infoPostFix": "",
+        "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+        "loadingRecords": "Cargando...",
+        "lengthMenu": "Mostrar _MENU_ registros",
+        "paginate": {
+            "first": "Primero",
+            "last": "Último",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        },
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "searchPlaceholder": "Término de búsqueda",
+        "zeroRecords": "No se encontraron resultados",
+        "emptyTable": "Ningún dato disponible en esta tabla",
+        "aria": {
+            "sortAscending":  ": Activar para ordenar la columna de manera ascendente",
+            "sortDescending": ": Activar para ordenar la columna de manera descendente"
+        },
+        //only works for built-in buttons, not for custom buttons
+        "buttons": {
+            "create": "Nuevo",
+            "edit": "Cambiar",
+            "remove": "Borrar",
+            "copy": "Copiar",
+            "csv": "fichero CSV",
+            "excel": "tabla Excel",
+            "pdf": "documento PDF",
+            "print": "Imprimir",
+            "colvis": "Visibilidad columnas",
+            "collection": "Colección",
+            "upload": "Seleccione fichero...."
+        },
+        "select": {
+            "rows": {
+                _: '%d filas seleccionadas',
+                0: 'clic fila para seleccionar',
+                1: 'una fila seleccionada'
+            }
+        }
+    }           
+} );
 </script>
+</div>
