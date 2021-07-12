f92f5941189a6ef4ecbbbd8bb4ae7f256ee1801c
d4lanis <daniel.alanis.hdz@gmail.com>
2021-07-12 17:35:47 -0500

    sin cambios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ColoresController;
 use App\Http\Controllers\RoboController;
 use App\Http\Controllers\VehiculoController;
 use App\Http\Controllers\DenuncianteController;
