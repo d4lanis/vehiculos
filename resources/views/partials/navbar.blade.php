34112e13c9850f82bdb3b65d70c99fa43e477c0d
d4lanis <daniel.alanis.hdz@gmail.com>
2021-08-10 16:32:34 -0500

    se agrego carpeta de partials y dentro un template de un navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,17 @@
+<!--Navbar-->
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Fiscalia General del Estado</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Inicio</a></li>
+        <li><a href="#">Vehiculos Robados</a></li>
+        <li><a href="#">Page 2</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="fas fa-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="fas fa sign.in-alt"></span> Login</a></li>
+      </ul>
+    </div>
+  </nav>
