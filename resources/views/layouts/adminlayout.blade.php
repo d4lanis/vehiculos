766ffdcb6ac6e93eda7d63e3a92dbd3cd17cba11
d4lanis <daniel.alanis.hdz@gmail.com>
2021-08-23 16:23:00 -0500

    se cambio bootstrap 5 por version 4 en el layout de admin

--- resources/views/layouts/adminlayout.blade.php
+++ resources/views/layouts/adminlayout.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vehiculos Robados y Recuperados</title>
-    <link rel="stylesheet" href="{{asset('bootstrap-5/css/bootstrap.min.css')}}" crossorigin="anonymous">
-    <script src="{{asset('bootstrap-5/js/bootstrap.min.js')}}" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{asset('bootstrap-4/css/bootstrap.min.css')}}" crossorigin="anonymous">
+    <script src="{{asset('bootstrap-4/js/bootstrap.min.js')}}" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{asset('font_awesome/css/font-awesome.min.css')}}">
     <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
     <link rel="stylesheet" type="text/css" href="{{asset('datatables/datatables.min.css')}}"/>
