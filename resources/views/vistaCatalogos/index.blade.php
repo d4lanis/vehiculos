6484478e8b3c2f372bea10b174bbbd5363681619
d4lanis <daniel.alanis.hdz@gmail.com>
2021-09-03 19:14:47 -0500

    index con tabla para mostrar los catalogos padre

--- resources/views/vistaCatalogos/index.blade.php
+++ resources/views/vistaCatalogos/index.blade.php
@@ -1,17 +1,45 @@
-@extends('layouts.layout')
+@extends('layouts.adminlayout')
 @section('content')
     <div class="container">
+        <br>
         <h2>Catalogos</h2>
-
-        <div class="d-inline-flex justify-content-around">
-            <a class="btn btn-success col-md-2">Entidades</a>
-            <a class="btn btn-success col-md-2">Marcas</a>
-            <a class="btn btn-success col-md-2">Colores</a>
-            <a class="btn btn-success col-md-2">Clases de Vehiculo</a>
-            <a class="btn btn-success col-md-2">Tipos de  Uso</a>
-            <a class="btn btn-success col-md-2">Tipos de Lugares</a>
-            <a class="btn btn-success col-md-2">Procedencias</a>
-            <a class="btn btn-success col-md-2">Modalidades</a>
-        </div>
+        <table id="catalogos" class="table table-light">
+            <thead class="thead-light">
+                <tr>
+                    <th>Nombre</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Entidades</td>
+                    <td><a href="vistaCatalogos.catalogo/1" class="btn btn-success">Ver</a></td>
+                </tr>
+                <tr>
+                    <td>Marcas</td>
+                    <td><a href="vistaCatalogos.catalogo/2" class="btn btn-success">Ver</a></td>
+                </tr>
+                <tr>
+                    <td>Colores</td>
+                    <td><a href="vistaCatalogos.catalogo/3" class="btn btn-success">Ver</a></td>
+                </tr>
+                <tr>
+                    <td>Clases de Vehiculo</td>
+                    <td><a href="vistaCatalogos.catalogo/4" class="btn btn-success">Ver</a></td>
+                </tr>
+                <tr>
+                    <td>Tipos de  Uso</td>
+                    <td><a href="vistaCatalogos.catalogo/5" class="btn btn-success">Ver</a></td>
+                </tr>
+                <tr>
+                    <td>Tipos de Lugares</td>
+                    <td><a href="vistaCatalogos.catalogo/6" class="btn btn-success">Ver</a></td>
+                </tr>
+                <tr>
+                    <td>Procedencias</td>
+                    <td><a href="vistaCatalogos.catalogo/7" class="btn btn-success">Ver</a></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 @endsection
