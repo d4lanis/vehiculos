c9ebbef6e414bf2311967080d8423d6be283bda5
d4lanis <daniel.alanis.hdz@gmail.com>
2021-09-15 18:47:25 -0500

    se agregaron algunos permisos, pero aun no se ha asignado nada a usuarios

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RoleSeeder extends Seeder
 {
@@ -16,5 +17,16 @@ class RoleSeeder extends Seeder
     {
         $role1 = Role::create(['name'=>'Admin']);
         $role2 = Role::create(['name'=>'Capturista']);
+
+        Permission::create('name','vehiculos_robados.index');
+        Permission::create('name','vehiculos_robados.create');
+        Permission::create('name','vehiculos_robados.edit');
+        Permission::create('name','vehiculos_robados.show');
+
+        Permission::create('name','admin.catalogos.index');
+        Permission::create('name','admin.catalogos.catalogo');
+        Permission::create('name','admin.catalogos.child');
+        
+        
     }
 }
