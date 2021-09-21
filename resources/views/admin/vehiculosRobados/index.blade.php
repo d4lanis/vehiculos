fd050fd2871777deef436a0113a74866287f2b8d
d4lanis <daniel.alanis.hdz@gmail.com>
2021-09-21 16:26:55 -0500

    se cambio el nombre de la carpeta de vehiculosRObadosAdmin a vehiculosRobados y se actualizaron las rutas

---
+++ resources/views/admin/vehiculosRobados/index.blade.php
@@ -0,0 +1,110 @@
+<head>
+    <link rel="stylesheet" href="{{asset('bootstrap-5/css/bootstrap.min.css')}}" crossorigin="anonymous">
+    <script src="{{asset('bootstrap-5/js/bootstrap.min.js')}}" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{asset('font_awesome/css/font-awesome.min.css')}}">
+    <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
+    <link rel="stylesheet" type="text/css" href="{{asset('datatables/datatables.min.css')}}"/>
+    <script type="text/javascript" src="{{asset('datatables/datatables.min.js')}}"></script>
+</head>
+
+
+<div class="container-fluid">
+    <h1>Denuncia de Vehiculos Robados</h1>
+    <!--<a href="#" class="btn btn-success">Nuevo</a>-->
+<br><br>
+<table id="index" class="table table-striped table-bordered" style="width: 100%;">
+    <thead class="table-info">
+        <tr>
+            <th data-priority="1">Id</th>
+            <th data-priority="2">Fecha/Hora</th>
+            <th data-priority="2">Municipio</th>
+            <th data-priority="2">Marca/Submarca</th>
+            <th>Modelo</th>
+            <th data-priority="1">Numero de Serie</th>
+            <th data-priority="1">Placa</th>
+            <th>Nombre Denunciante</th>
+            <th data-priority="1">Acciones</th>
+        </tr>
+    </thead>
+</table>
+
+<script>
+    $(document).ready( function () {
+        $('#index').DataTable({
+            "processing": true,
+            "serverSide": true,
+            "responsive": true,
+            "lengthMenu": [[5,10,50,100],[5,10,50,100]],
+            "ajax": "/fillData",
+            "columns": [
+                {data: 'id', orderable: false, searchable: false},
+                {data: 'dateAveriguacion', orderable: true, searchable: true},
+                {data: 'municipio', orderable: true, searchable: false},
+                {data: 'marca', orderable: true, searchable: true},
+                {data: 'modelo', orderable: true, searchable: true},
+                {data: 'numSerie', orderable: true, searchable: true},
+                {data: 'placa', orderable: true, searchable: true},
+                {data: 'nombre', orderable: true, searchable: true},
+                {data: 'acciones', name:'acciones', searchable:false, orderable:false,
+                     render: function(data,style,row,meta){
+                         return $("<div/>").html(data).text();
+                     }
+                }
+            ],
+            "columnDefs": 
+            [
+                { responsivePriority: 1, targets: 1 },
+                { responsivePriority: 2, targets: -2 }
+            ]          
+        });
+    });
+    $.extend( true, $.fn.dataTable.defaults, {
+    "language": {
+        "decimal": ",",
+        "thousands": ".",
+        "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "infoPostFix": "",
+        "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+        "loadingRecords": "Cargando...",
+        "lengthMenu": "Mostrar _MENU_ registros",
+        "paginate": {
+            "first": "Primero",
+            "last": "Último",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        },
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "searchPlaceholder": "Término de búsqueda",
+        "zeroRecords": "No se encontraron resultados",
+        "emptyTable": "Ningún dato disponible en esta tabla",
+        "aria": {
+            "sortAscending":  ": Activar para ordenar la columna de manera ascendente",
+            "sortDescending": ": Activar para ordenar la columna de manera descendente"
+        },
+        //only works for built-in buttons, not for custom buttons
+        "buttons": {
+            "create": "Nuevo",
+            "edit": "Cambiar",
+            "remove": "Borrar",
+            "copy": "Copiar",
+            "csv": "fichero CSV",
+            "excel": "tabla Excel",
+            "pdf": "documento PDF",
+            "print": "Imprimir",
+            "colvis": "Visibilidad columnas",
+            "collection": "Colección",
+            "upload": "Seleccione fichero...."
+        },
+        "select": {
+            "rows": {
+                _: '%d filas seleccionadas',
+                0: 'clic fila para seleccionar',
+                1: 'una fila seleccionada'
+            }
+        }
+    }           
+} );
+</script>
+</div>
---
+++ resources/views/admin/vehiculosRobados/show.blade.php
@@ -0,0 +1,228 @@
+<head>
+    <link rel="stylesheet" href="{{asset('bootstrap-5/css/bootstrap.min.css')}}" crossorigin="anonymous">
+    <script src="{{asset('bootstrap-5/js/bootstrap.min.js')}}" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{asset('font_awesome/css/font-awesome.min.css')}}">
+    <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
+    <link rel="stylesheet" type="text/css" href="{{asset('datatables/datatables.min.css')}}"/>
+    <script type="text/javascript" src="{{asset('datatables/datatables.min.js')}}"></script>
+    <script src="{{asset('js/navegacion.js')}}"></script>
+  </head>
+
+<div class="container">
+    <br>
+    <h1>Denuncia de Vehiculos Robados</h1>
+  <ul class="nav nav-tabs nav-fill mb-3" role="tablist">
+    <li class="nav-item active" role="presentation">
+      <a class="nav-link active" href="#paso1" aria-controls="paso1" role="tab" data-bs-toggle="tab" aria-expanded="true">Información del Robo</a>
+    </li>
+    <li class="nav-item" role="presentation">
+      <a class="nav-link" href="#paso2" aria-controls="paso2" role="tab" data-bs-toggle="tab" aria-expanded="false">Información del Vehiculo</a>
+    </li>
+    <li class="nav-item" role="presentation">
+      <a class="nav-link" href="#paso3" aria-controls="paso3" role="tab" data-bs-toggle="tab" aria-expanded="false">Informacion del Denunciante</a>
+    </li>
+  </ul>
+
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="paso1">
+        @include('robos.form',['modo'=>'Ver']);
+        <div class="pull-right">
+          <a class="btn btn-primary continue" id="continue">Pagina siguiente</a>
+          <a class="btn btn-warning" href="{{route('vehiculos_robados.index')}}">Regresar</a>
+        </div>
+    </div>
+
+    <div role="tabpanel" class="tab-pane" id="paso2">
+        @include('vehiculos.form',['modo'=>'Ver']);
+        <div class="pull-right">
+          <a class="btn btn-primary back">Pagina anterior</a>
+          <a class="btn btn-primary continue">Pagina Siguiente</a>
+          <a class="btn btn-warning" href="{{route('vehiculos_robados.index')}}">Regresar</a>
+        </div>
+    </div>
+
+    <div role="tabpanel" class="tab-pane" id="paso3">
+        @include('denunciantes.form',['modo'=>'Ver']);
+        <div class="pull-right">
+          <a class="btn btn-primary back">Pagina anterior</a>
+          <a class="btn btn-warning" href="{{route('vehiculos_robados.index')}}">Regresar</a>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+      $(document).ready(function() {
+  
+          
+          $.ajaxSetup({
+              headers: {
+              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          }});
+      
+          function dynamicDropdown(url, id = null, target, otro = null) {
+
+          let data = {
+              "_token": "{{ csrf_token() }}",
+              id: id
+          }
+  
+          $.post(url, data, function(result){
+              let default_value = 0;
+              let $select = $("#"+target);
+              $select.empty();
+              let options = [];
+  
+              if(null == data.id){
+                  options.push(`<option value="" selected>`+
+                      ` - seleccione una opción</option>`);
+              } else {
+                  options.push(`<option value=""> -------------- </option>`);
+                  default_value = data.id;
+              }
+  
+              if(result.status === 'ok'){
+                  $.each(result.data, function(i, item) {
+                      item_name = item.name.toUpperCase();
+                      if(item.id == default_value){
+                          options.push(`<option value="${item.id}" selected>${item_name}</option>`);
+                      } else {
+                          options.push(`<option value="${item.id}">${item_name}</option>`);
+                      }
+  
+                  });
+                  if (null != otro) {
+                       options.push(`<option value="1">`+otro+`</option>`);
+                  }
+              }
+              
+              $select.append(options);
+  
+          }).fail(function(){
+              $('#getDiv').html('algo salio mal');
+          });
+      }
+  
+      function clearDropdown(select)
+      {
+          select.empty();
+          let options = [];
+          options.push(`<option value="" disabled selected> Cargando datos </option>`);
+          select.append(options);
+      }
+          //form robos
+          dynamicDropdown("/get_estados", {{ old('entidad_id',isset($robo->entidad_id)?$robo->entidad_id: 0) ??  0 }}, 'entidad_id');
+          dynamicDropdown('/get_municipios/'+{{ old('entidad_id',isset($robo->entidad_id)?$robo->entidad_id:0) ?? 0 }}, 
+              {{ old('municipio_id',isset($robo->municipio_id)?$robo->municipio_id: 0) ?? 0 }}, 'municipio_id');
+          dynamicDropdown('/get_poblaciones/'+{{ old('entidad_id',isset($robo->entidad_id)?$robo->entidad_id: 0) ?? 0 }}+'/'+{{ old('municipio_id',isset($robo->municipio_id)?$robo->municipio_id:0) ?? 0 }}, 
+              {{ old('localidad_id',isset($robo->localidad_id)?$robo->localidad_id:0) ?? 0 }}, 'localidad_id');
+          dynamicDropdown("/get_tipolugar", {{ old('tipoLugar_id',isset($robo->tipoLugar_id)?$robo->tipoLugar_id: -1) ?? -1}}, 'tipoLugar_id');
+          dynamicDropdown("/get_modalidad", {{ old('modalidad_id',isset($robo->modalidad_id)?$robo->modalidad_id:-1) ?? -1}}, 'modalidad_id');
+          
+          //form vehiculos
+          dynamicDropdown("/get_marcas", {{ old('marca_id',isset($vehiculo->marca_id)?$vehiculo->marca_id:-1)?? -1 }}, 'marca_id');
+          dynamicDropdown('/get_submarcas/'+{{ old('marca_id',isset($vehiculo->marca_id)?$vehiculo->marca_id:-1) ?? -1 }}, 
+            {{ old('subMarca_id',isset($vehiculo->subMarca_id)?$vehiculo->subMarca_id:-1) ?? -1 }}, 'subMarca_id');
+          dynamicDropdown("/get_colores", {{ old('color_id',isset($vehiculo->color_id)?$vehiculo->color_id:-1)?? -1 }}, 'color_id');
+          dynamicDropdown("/get_tipousos", {{ old('tipoUso_id',isset($vehiculo->tipoUso_id)?$vehiculo->tipoUso_id:-1)?? -1 }}, 'tipoUso_id');
+          dynamicDropdown("/get_procedencias", {{ old('procedencia_id',isset($vehiculo->procedencia_id)?$vehiculo->procedencia_id:-1)?? -1 }}, 'procedencia_id');
+          dynamicDropdown("/get_clasevehiculos", {{ old('claseVehiculo_id',isset($vehiculo->claseVehiculo_id)?$vehiculo->claseVehiculo_id:-1)?? -1 }}, 'claseVehiculo_id');
+          dynamicDropdown('/get_tipovehiculos/'+{{ old('claseVehiculo_id',isset($vehiculo->claseVehiculo_id)?$vehiculo->claseVehiculo_id:-1) ?? -1 }}, 
+            {{ old('tipoVehiculo_id',isset($vehiculo->tipoVehiculo_id)?$vehiculo->tipoVehiculo_id:-1) ?? -1 }}, 'tipoVehiculo_id');
+
+          //form denunciantes
+          dynamicDropdown("/get_estados/", {{ old('entidad_idD',isset($denunciante->entidad_id)?$denunciante->entidad_id:0) ?? 0 }}, 'entidad_idD');
+          dynamicDropdown('/get_municipios/'+{{ old('entidad_idD',isset($denunciante->entidad_id)?$denunciante->entidad_id:0) ?? 0 }}, 
+          {{ old('municipio_idD',isset($denunciante->municipio_id)?$denunciante->municipio_id:0) ?? 0 }}, 'municipio_idD');
+          
+          $('select[name="entidad_id"]').change(function(e){
+              clearDropdown( $('select[name="municipio_id"]') );
+              clearDropdown( $('select[name="localidad_id"]') );
+              var optionId = $('select[name="entidad_id"] option:selected').val();
+              //$('#entidad').val($('#entidad_id :selected').text());
+              dynamicDropdown('/get_municipios/'+optionId, 0, 'municipio_id');
+          });
+
+      
+          $('select[name="municipio_id"]').change(function(e){
+              clearDropdown( $('select[name="localidad_id"]') );
+              var entidadId = $('select[name="entidad_id"] option:selected').val();
+              var municipioId = $('select[name="municipio_id"] option:selected').val();
+              //$('#municipio').val($('#municipio_id :selected').text());
+              dynamicDropdown('/get_poblaciones/'+entidadId+'/'+municipioId, 0, 'localidad_id');
+              
+          });
+  
+          $('select[name="localidad_id"]').change(function(e){
+              var optionId = $('select[name="localidad_id"] option:selected').val();
+              //$('#localidad').val($('#localidad_id :selected').text());
+          });
+
+          $('select[name="tipoLugar_id"]').change(function(e){
+              var optionId = $('select[name="tipoLugar_id"] option:selected').val();
+              //$('#tipoLugar').val($('#tipoLugar_id :selected').text());
+          });
+
+          $('select[name="modalidad_id"]').change(function(e){
+              var optionId = $('select[name="modalidad_id"] option:selected').val();
+              //$('#modalidad').val($('#modalidad_id :selected').text()); 
+          });
+
+          $('select[name="marca_id"]').change(function(e){
+            clearDropdown( $('select[name="subMarca_id"]') );
+            clearDropdown( $('select[name="subMarca_id"]') );
+            var optionId = $('select[name="marca_id"] option:selected').val();
+            //$('#marca').val($('#marca_id :selected').text());
+            dynamicDropdown('/get_submarcas/'+optionId, -1, 'subMarca_id');
+        });
+
+        $('select[name="subMarca_id"]').change(function(e){
+            var optionId = $('select[name="marca_id"] option:selected').val();
+            //$('#subMarca').val($('#subMarca_id :selected').text());
+        });
+
+        $('select[name="color_id"]').change(function(e){
+            var optionId = $('select[name="color_id"] option:selected').val();
+            //$('#color').val($('#color_id :selected').text());
+        });
+
+        $('select[name="claseVehiculo_id"]').change(function(e){
+            clearDropdown( $('select[name="tipoVehiculo_id"]') );
+            clearDropdown( $('select[name="tipoVehiculo_id"]') );
+            var optionId = $('select[name="claseVehiculo_id"] option:selected').val();
+            //$('#claseVehiculo').val($('#claseVehiculo_id :selected').text());
+            dynamicDropdown('/get_tipovehiculos/'+optionId, -1, 'tipoVehiculo_id');
+        });
+
+        $('select[name="tipoVehiculo_id"]').change(function(e){
+            var optionId = $('select[name="claseVehiculo"] option:selected').val();
+            //$('#tipoVehiculo').val($('#tipoVehiculo_id :selected').text());
+        });
+
+        $('select[name="tipoUso_id"]').change(function(e){
+            var optionId = $('select[name="tipoUso_id"] option:selected').val();
+            //$('#tipoUso').val($('#tipoUso_id :selected').text());
+        });
+
+        $('select[name="procedencia_id"]').change(function(e){
+            var optionId = $('select[name="procedencia_id"] option:selected').val();
+            //$('#procedencia').val($('#procedencia_id :selected').text());
+        });
+
+        $('select[name="entidad_idD"]').change(function(e){
+          clearDropdown( $('select[name="municipio_idD"]') );
+          clearDropdown( $('select[name="localidad_idD"]') );
+          var optionId = $('select[name="entidad_idD"] option:selected').val();
+          //$('#entidadD').val($('#entidad_idD :selected').text());
+          dynamicDropdown('/get_municipios/'+optionId, 0, 'municipio_idD');
+        });
+
+        $('select[name="municipio_idD"]').change(function(e){
+          clearDropdown( $('select[name="localidad_idD"]') );
+          var optionId = $('select[name="municipio_idD"] option:selected').val();
+          //$('#municipioD').val($('#municipio_idD :selected').text());
+        });
+
+      });
+  </script>
+</div>
+</div>
+<div id="push"></div>
